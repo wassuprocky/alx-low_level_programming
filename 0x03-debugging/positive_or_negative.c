28782b63b4ff76e6c0672c19c235bcbd69e6b8a0
root <root@nyasetia>
2023-10-22 17:50:23 +0300

    Removing an infinite loop

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,23 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
 #include "main.h"
 /**
-  * main - check if a number is odd even or zero
-  * Return:Always 0 if the operation was successful
+  * main - check if a number if equal to zero,poitive or negative
+  * Return:Always(0)
   */
 
-
-positive_or_negative(void)
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if(n==0)
-		printf("is zero");
-	else if(n>0){	
-		printf("is positive");
+	if (i>0)
+		printf("%d is positive",i);
+	else if (i<0)
+		printf("%d is negative",i);
 	else
-		printf("is negative");
-	return (0);
+		printf("%d is zero",i);
 }
