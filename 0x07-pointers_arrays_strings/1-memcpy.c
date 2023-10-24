8f411b25f15a95a1d972c0e28f3070e3880db957
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-23 18:50:20 -0700

    Used a while loop  instead of a for loop and did some editing _betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- **_memcpy - The main function for alllocation
+ **_memcpy - The main function for allocation of memeory from @ source to @dest in @n number of times
+ *
  *@dest: The destination address
+ *
  *@src: Source
+ *
  *@n : Maximut times to be added
+ *
+ * Return : pointer to @dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	for(i = 0; i < n; i++)
+	unsigned int i = 0;
+			
+	while (i < n)
 	{
 		dest[i]=src[i];
+		i++;
 	}
 	return (dest);
 }
