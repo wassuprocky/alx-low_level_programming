cc94bc5bbd49923e02133ebc2bf2481aa6547a80
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-23 18:58:08 -0700

    betty edit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- **_memcpy - The main function for allocation of memeory from @ source to @dest in @n number of times
+ * _memcpy - The main function for allocation of
+ * memeory from @ source to @dest in @n number of times
  *
- *@dest: The destination address
+ * @dest: The destination address
  *
- *@src: Source
+ * @src: Source
  *
- *@n : Maximut times to be added
+ * @n: Maximut times to be added
  *
- * Return : pointer to @dest
+ * Return: pointer to @dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
-			
 	while (i < n)
 	{
-		dest[i]=src[i];
+		dest[i] = src[i];
 		i++;
 	}
+
 	return (dest);
 }
