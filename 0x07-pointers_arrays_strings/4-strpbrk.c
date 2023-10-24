52869211059ba00024caf943560e1d782b3f051e
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-24 15:54:51 -0700

    Letters that match and return the pointer address

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for ( i = 0; s[i] != '\0'; i++)
+	{
+		for ( j = 0; accept[j] != '\0'; j++)
+		{
+			if ( s[i] == accept[j])
+				return &s[i];
+		}
+	}
+	return (NULL);
+}
