f459c1fb7d9745306e4481d0a95e432aebc53c5a
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-23 17:56:55 -0700

    Creation of a file that fills the size of bytes provided

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ **_memset -Fills the memeory witha constant byte
+ *@s: Refers to the pointer
+ *@b: Refers to the character used
+ *@n: Refers to the number of maximum times to run 
+ * Return:s when successful.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for( i = 0;i < n;i++)
+	{
+		s[i]=b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is a reamdme of a file explaining more and more about pointers
+and multidimenional arrays
