6d44d05d03b020fba6151f96d3953fa15015cc9c
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-24 15:15:32 -0700

    Commit the checking of the number of bytes that actually match

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * _strspn - Main function of checking for number of bytes
- *
- * @s: Main string
- * 
- * @accept: String to check
- *
- * Return: number of times
- */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
-	unsigned int total = 0;
+    unsigned int i, j, count;
 
-	for (i = 0; s[i] >= '\0'; i++)
-	{
-		for (int j = 0; accept[j] >= '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				total += 1;
-			}
-		}
-	}
-	return (total);
+    count = 0;
+    for (i = 0; s[i]; i++)
+    {
+        for (j = 0; accept[j]; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                count++;
+                break;
+            }
+        }
+        if (!accept[j])
+            break;
+    }
+    return (count);
 }
+
