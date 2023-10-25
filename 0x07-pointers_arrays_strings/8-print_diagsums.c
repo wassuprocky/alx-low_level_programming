4324b03400ec09541183e3a071f099431914fd22
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-24 17:21:15 -0700

    structure of multi_dim

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_diagsums(int *a, int size)
+{
+	unsigned int sum;
+        int i;
+	unsigned int sum_2;
+	
+	for (i = 0; i < size; i++)
+	{
+		sum += a[i*size+i];
+		sum_2 += a[i*size-i];
+	}
+	printf("%d%d",sum,sum_2); 
+
+}
