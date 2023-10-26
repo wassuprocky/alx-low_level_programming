826c2d99090c905f042d3e68905b286fc1f07bb4
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-25 18:51:13 -0700

    added the necessary

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,17 +1,43 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - searches a string for any set of bytes
+ * @s: string to be searched
+ * @accept: string to be used
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes in accept
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i;
-	unsigned int j;
+	int i = 0;
 
-	for ( i = 0; s[i] != '\0'; i++)
-	{
-		for ( j = 0; accept[j] != '\0'; j++)
-		{
-			if ( s[i] == accept[j])
-				return &s[i];
-		}
-	}
-	return (NULL);
+	for (; s[i] != '\0' && _strchr(accept, s[i]) == NULL; i++)
+		;
+
+	if (s[i] == '\0')
+		return (NULL);
+	else
+		return (s + i);
+}
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string to be searched
+ * @c: char to be checked
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != c && s[i] != '\0'; i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,16 +1,23 @@
+#include <stdio.h>
 #include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: array to be used
+ * @size: size of the matrix
+ *
+ * Return: void
+ */
 
 void print_diagsums(int *a, int size)
 {
-	unsigned int sum;
-        int i;
-	unsigned int sum_2;
-	
-	for (i = 0; i < size; i++)
-	{
-		sum += a[i*size+i];
-		sum_2 += a[i*size+(size-i-1)];
-	}
-	printf("%d, %d",sum,sum_2); 
+	int i = 0, j = 0, sum_1 = 0, sum_2 = 0;
 
+	for (; i < size * size; i += size)
+	{
+		sum_1 += a[i + j];
+		sum_2 += a[i + (size - 1) - j];
+		j++;
+	}
+	printf("%d, %d\n", sum_1, sum_2);
 }
