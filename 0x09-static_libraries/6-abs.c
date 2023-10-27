ca2d4eee78c47c7f34487829aff40896aeb2e513
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-27 14:48:00 -0700

    some

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,2 +1,27 @@
 #include "main.h"
-int _abs(int n);
+
+
+/**
+ * _abs - This is a absolute value
+ *
+ * @a: This is the entry
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int a)
+{
+	if (a > 0)
+	{
+		return (a);
+	}
+	else if (a < 0)
+	{
+		a = a * -1;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
