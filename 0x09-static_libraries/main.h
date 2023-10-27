fe025b66da0ef8aab5a53d4a22b01d22727ab4d3
Arnold Nyasetia <nyasetiaarnold@gmail.com>
2023-10-27 14:28:37 -0700

    something of the main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 int _putchar(char c);
 int _islower(int c);
@@ -20,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
